1dc24a2de529943b88a747bd982bdf83ee8bc582
Shivali Sachan <shiva@example.com>
2026-08-19 23:33:18 +0530

    Initial Commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,74 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+export const AppContext = createContext();
+
+const currency = import.meta.VITE_CURRENCY;
+
+export const AppContextProvider = ({children})=>{
+      const navigate = useNavigate();
+      const [user, setUser] = useState(null)
+      const [isSeller, setIsSeller] = useState(false)
+      const [showUserLogin, setShowUserLogin] = useState(false)
+      const [products, setProducts] = useState([])
+
+      const [cartItems, setCartItems] = useState({})
+
+      // Fetch All Products
+      const fetchProducts = async ()=>{
+        setProducts(dummyProducts)
+      }
+
+      //Add product to cart
+      const addToCart = ()=>{
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]){
+          cartData[itemId] += 1;
+        }else{
+          cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Added to Cart")
+      }
+
+      //Update Cart Item Quantity
+      const updateCartItem = (itemId, quantity)=>{
+        let cartData = structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cartData)
+        toast.success("Cart Updated")
+      }
+
+      //Remove Product from Cart
+      const removeFromCart = (itemId)=>{
+         let cartData = structuredClone(cartItems);
+         if(cartData[itemId]){
+          cartData[itemId] -= 1;
+          if(cartData[itemId] === 0){
+            delete cartData[itemId];
+          }
+         }
+         toast.success("Removed from Cart");
+         setCartItems(cartData)
+      }
+
+      useEffect(()=>{
+          fetchProducts()
+      },[])
+
+      const value = {navigate, user, setUser, setIsSeller,
+                    isSeller, showUserLogin, setShowUserLogin,
+                    products, currency, addToCart, updateCartItem, removeFromCart
+                  }
+
+      return <AppContext.Provider value={value}>
+        {children}
+      </AppContext.Provider>
+}
+
+export const useAppContext = () =>{
+   return useContext(AppContext)
+}
