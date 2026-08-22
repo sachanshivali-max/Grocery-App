6452fd73b2b1e8de6ce1aa2e9718100b22acd5bc
Shivali Sachan <shiva@example.com>
2026-08-22 13:56:56 +0530

    Add Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import {Toaster } from "react-hot-toast";
 import Footer from './components/Footer';
 import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
+import AllProducts from './pages/AllProducts';
+import ProductCatgory from './pages/ProductCatgory';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
 
 const App = () => {
 
@@ -22,6 +26,11 @@ const App = () => {
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<AllProducts/>} />
+          <Route path='/products/:category' element={<ProductCatgory/>} />
+          <Route path='/products/:category/:id' element={<ProductDetails/>} />
+          <Route path='/cart' element={<Cart/>} />
+
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -15,6 +15,9 @@ export const AppContextProvider = ({children})=>{
       const [products, setProducts] = useState([])
 
       const [cartItems, setCartItems] = useState({})
+      const [searchQuery, setSearchQuery] = useState({})
+      
+
 
       // Fetch All Products
       const fetchProducts = async ()=>{
@@ -55,6 +58,27 @@ export const AppContextProvider = ({children})=>{
          setCartItems(cartData)
       }
 
+      // Get cart item count
+      const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const item in cartItems){
+          totalCount += cartItems[item]
+        }
+        return totalCount;
+      }
+
+      //Get cart total Amount
+      const getCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const items in cartItems){
+          let itemInfo = products.find((product)=> product._id === items);
+          if(cartItems[items] > 0){
+            totalAmount += itemInfo.offerPrice * cartItems[items]
+          }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+      }
+
       useEffect(()=>{
           fetchProducts()
       },[])
@@ -62,7 +86,7 @@ export const AppContextProvider = ({children})=>{
       const value = {navigate, user, setUser, setIsSeller,
                     isSeller, showUserLogin, setShowUserLogin,
                     products, currency, addToCart, updateCartItem, removeFromCart,
-                    cartItems
+                    cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount
                   }
 
       return <AppContext.Provider value={value}>
