8c929b0e33048991561528d5c0007851042b2379
Shivali Sachan <shiva@example.com>
2026-09-03 00:07:02 +0530

    Add some Backend files in server folder

---
+++ server.js
@@ -0,0 +1,31 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+await connectDB();
+await connectCloudinary();
+
+//Allow mutiple origins
+const allowedOrigins = ['http://localhost:5173']
+
+//Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}))
+
+app.get('/', (req, res) => res.send("API is working"));
+app.use('/api/user', userRouter);
+app.use('/api/seller', sellerRouter);
+
+
+app.listen(port, ()=>{
+      console.log(`Server is running on http://localhost:${port}`);
+})
